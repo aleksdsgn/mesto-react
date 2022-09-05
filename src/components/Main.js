b048a9f24a2bc2658137504c43d98dbc68b7446e
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-09-05 14:45:17 +0700

    download user data from server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react';
+import { api } from '../utils/api.js';
 import avatar from '../images/avatar-base.jpg';
 
 function Main(props) {
+  const [userAvatar, setUserAvatar] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userDescription,setUserDescription] = useState("");
+
+  useEffect(() => {
+    Promise.all([
+      api.getProfileInfo(),
+      // api.getInitialCards(),
+    ])
+    .then(([userInfo, cards]) => {
+      setUserAvatar(userInfo.avatar);
+      setUserName(userInfo.name);
+      setUserDescription(userInfo.about);
+      // newUserInfo.setUserInfo(userInfo);
+      // userId = userInfo._id;
+
+      // cardsList.renderItems(cards);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
   return (
     <main className="content">
 
@@ -11,21 +36,22 @@ function Main(props) {
           type="button"
         >
           <img
-            src={avatar}
+            src={userAvatar}
             alt="Аватар профиля"
             className="profile__avatar"
+            style={{ backgroundImage: `url(${userAvatar})` }}
           />
         </button>
         <div className="profile__info">
           <div className="profile__title-with-button">
-            <h1 className="profile__title">Имя</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button
               onClick={props.onEditProfile}
               className="profile__button profile__button_type_edit-profile-info"
               type="button"
             />
           </div>
-          <p className="profile__subtitle">О себе</p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button
           onClick={props.onAddPlace}
