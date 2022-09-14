418338ebc1ed85895b851cb4809faa3d3cad6a85
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-09-14 18:18:15 +0700

    feat: use context current user in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,26 @@
+import { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 function Card({ card, onCardClick }) {
+  // подписка на контекст
+  const currentUser = useContext(CurrentUserContext);
+
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = card.owner._id === currentUser._id;
+
+  // Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = (
+    `${isOwn ? 'card__button-delete' : ''}`
+  );
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+    `card__button-like ${isLiked ? 'card__button-like_active' : ''}`
+  );
+
   function handleCardClick() {
     onCardClick(card);
   }
@@ -6,7 +28,7 @@ function Card({ card, onCardClick }) {
   return (
     <li className="places__element">
       <article className="card">
-        <button className="card__button-delete" type="button" aria-label="Удалить" />
+        <button className={cardDeleteButtonClassName} type="button" aria-label="Удалить" />
         {/* eslint-disable */}
         <img
           className="card__image"
@@ -19,7 +41,7 @@ function Card({ card, onCardClick }) {
         <div className="card__info">
           <h2 className="card__title">{card.name}</h2>
           <div className="card__like-container">
-            <button className="card__button-like" type="button" aria-label="Нравится" />
+            <button className={cardLikeButtonClassName} type="button" aria-label="Нравится" />
             <p className="card__likes-counter">{card.likes.length}</p>
           </div>
         </div>
