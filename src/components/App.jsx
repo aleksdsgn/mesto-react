7f30795ca87c41e2cf4a6a4bb59c1978c45ef126
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-09-22 21:12:51 +0700

    fix: install error handlers for requests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -87,9 +87,13 @@ function App() {
     // Снова проверяем, есть ли уже лайк на этой карточке
     const isLiked = card.likes.some((i) => i._id === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
-    api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
-      setCards((state) => state.map((c) => ((c._id === card._id) ? newCard : c)));
-    });
+    api.changeLikeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((state) => state.map((c) => ((c._id === card._id) ? newCard : c)));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   // Открытие попапа редактирования аватара
@@ -124,6 +128,9 @@ function App() {
       .then(() => {
         setCards((state) => state.filter((c) => c._id !== selectedCard._id));
         closeAllPopups();
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
 
